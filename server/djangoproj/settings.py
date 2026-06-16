c09bf37914863dd2a7630ed87fee630c6e527e4b
rodrigcasio <rod.casio@outlook.com>
2026-06-15 21:12:16 -0600

    `fix:` lint python errors

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -22,20 +22,25 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0"
+SECRET_KEY = (
+    "django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
-     ".theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",
+    ".theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",
     ".theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai",
 ]
 CSRF_TRUSTED_ORIGINS = [
-    "https://rodcasio-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",
-    "https://*.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",
-    "https://*.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai",
+    "https://rodcasio-8000.theiadockernext-1-labs-prod-theiak8s-4"
+    "-tor01.proxy.cognitiveclass.ai",
+    "https://*.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy"
+    ".cognitiveclass.ai",
+    "https://*.theianext-1-labs-prod-misc-tools-us-east-0.proxy"
+    ".cognitiveclass.ai",
 ]
 
 REST_FRAMEWORK = {
@@ -100,16 +105,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME": "django.contrib.auth.password_validation"
+                ".UserAttributeSimilarityValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "NAME": "django.contrib.auth.password_validation"
+                ".MinimumLengthValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        "NAME": "django.contrib.auth.password_validation"
+                ".CommonPasswordValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        "NAME": "django.contrib.auth.password_validation"
+                ".NumericPasswordValidator",
     },
 ]
 
