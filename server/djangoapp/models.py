75048eda316ed9cc8118df206dc126c910639696
rodrigcasio <rod.casio@outlook.com>
2026-06-15 21:01:29 -0600

    `fix:` fix linting errors

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -14,27 +13,29 @@ class CarMake(models.Model):
         return self.name  # Return the name as the string representation
 
 
-
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
-    dealer_id = models.IntegerField()  # <- instructions asked this missing field
+    # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    dealer_id = models.IntegerField()
     name = models.CharField(max_length=100)
-    
+
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon'),
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    
-    # Updated default to 2026 and MaxValueValidator to 2026 to avoid validation errors
-    year = models.IntegerField(default=2026,
+
+    # Updated default to 2026 to avoid validation errors
+    year = models.IntegerField(
+        default=2026,
         validators=[
             MaxValueValidator(2026),
             MinValueValidator(2015)
-        ])
+        ]
+    )
 
     def __str__(self):
-        return self.car_make.name + " " + self.name  # Returns full name (e.g. "Toyota Corolla")
+        return self.car_make.name + " " + self.name
 
