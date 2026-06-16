f6a64ecbb78b8c9cbc1f4c1f5113e406153e144b
rodrigcasio <rod.casio@outlook.com>
2026-06-15 21:13:20 -0600

    `fix:` lint python errors

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -26,11 +26,15 @@ urlpatterns = [
     path("", TemplateView.as_view(template_name="Home.html")),
     path("about/", TemplateView.as_view(template_name="About.html")),
     path("contact/", TemplateView.as_view(template_name="Contact.html")),
-    path("login/", TemplateView.as_view(template_name="index.html")),
-    path("register/", TemplateView.as_view(template_name="index.html")),
-    path("dealers/", TemplateView.as_view(template_name="index.html")),
-    path("dealer/<str:dealer_id>", TemplateView.as_view(template_name="index.html")),
+    path('login/', TemplateView.as_view(template_name="index.html")),
+    path('register/', TemplateView.as_view(template_name="index.html")),
+    path('dealers/', TemplateView.as_view(template_name="index.html")),
     path(
-        "postreview/<str:dealer_id>", TemplateView.as_view(template_name="index.html")
+        'dealer/<str:dealer_id>',
+        TemplateView.as_view(template_name="index.html")
+    ),
+    path(
+        'postreview/<str:dealer_id>',
+        TemplateView.as_view(template_name="index.html")
     ),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
